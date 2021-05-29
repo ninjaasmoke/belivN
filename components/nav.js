6a3e759cb322480478463e4c49ea0230218c3ef3
Nithin Sai K J <nithins674@gmail.com>
2021-05-29 21:41:06 +0530

    added boilerplate

---
+++ components/nav.js
@@ -0,0 +1,14 @@
+import navStyles from './nav.module.css'
+
+export default function Nav() {
+    return (
+        <nav className={navStyles.nav}>
+            <h1>beliv</h1>
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
