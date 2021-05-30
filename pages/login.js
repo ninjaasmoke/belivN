df92e2f11837e6ce8461adbedc5b75eaaaed8ac5
Nithin Sai K J <nithins674@gmail.com>
2021-05-30 20:05:00 +0530

    added google signin

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Login.module.css'
+import { GoogleLogin } from 'react-google-login';
 
 export default function Login() {
     return (
@@ -8,6 +9,18 @@ export default function Login() {
                 <p className={styles.desc}>
                     Please login to use <span>beliv</span>.
                 </p>
+                <GoogleLogin
+                    clientId="422317457275-32trbdq50ku5hu36qr13b0oa9tb0cs8o.apps.googleusercontent.com"
+                    onSuccess={(resp) => { console.log(resp) }}
+                    onFailure={(resp) => { console.log(resp) }}
+                    cookiePolicy="single_host_origin"
+                    isSignedIn={true}
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} disabled={renderProps.disabled} className={styles.loginB}>
+                            <img src="./images/g_icon.png" alt="Google Icon" className={styles.loginI} /> Log in with Google
+                        </button>
+                    )}
+                />
             </div>
         </div>
     )
