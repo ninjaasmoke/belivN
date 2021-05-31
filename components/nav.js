3141ffd24f332cf3bfbae05ca2b75575c2a93a6b
Nithin Sai K J <nithins674@gmail.com>
2021-05-31 21:29:10 +0530

    edited nav

--- components/nav.js
+++ components/nav.js
@@ -16,8 +16,8 @@ export default function Nav() {
             <div className={navStyles.popUpWrapper} style={{ display: showMenu ? 'block' : 'none' }} onClick={() => setShowMenu(false)}>
                 <div className={navStyles.popUp}>
                     <ul>
-                        <li><img src="./images/profile_icon.png" /> Profile</li>
-                        <li><img src="./images/settings_icon.png" /> Settings</li>
+                        <Link href="/profile"><li><img src="./images/profile_icon.png" /> Profile</li></Link>
+                        <Link href="/settings"><li><img src="./images/settings_icon.png" /> Settings</li></Link>
                     </ul>
                 </div>
             </div>
